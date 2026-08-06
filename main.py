bb74705cf86a9e7fa1c1f13e1eb810ae28c34077
Aprakash-ai <adityaprakash9692@gmail.com>
2026-08-06 13:46:42 +0530

    feat: implement feature scaling using StandardScaler

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 from src.data_loader import load_dataset
 from src.preprocessing import preprocess_data
-from src.train import split_data
+from src.train import split_data, scale_features
 
 
 def main():
 
+    # Load Dataset
     df = load_dataset()
 
+    # Preprocess Dataset
     X, y = preprocess_data(df)
 
+    # Train-Test Split
     X_train, X_test, y_train, y_test = split_data(X, y)
 
-    print("=" * 70)
-    print("TRAINING SET")
-    print("=" * 70)
-
-    print("X_train :", X_train.shape)
-    print("y_train :", y_train.shape)
-
-    print()
+    # Feature Scaling
+    X_train, X_test, scaler = scale_features(X_train, X_test)
 
     print("=" * 70)
-    print("TESTING SET")
+    print("TRAINING DATA")
     print("=" * 70)
+    print(X_train.head())
 
-    print("X_test  :", X_test.shape)
-    print("y_test  :", y_test.shape)
+    print("\nTraining Shape :", X_train.shape)
+
+    print("\n" + "=" * 70)
+    print("TEST DATA")
+    print("=" * 70)
+    print(X_test.head())
+
+    print("\nTesting Shape :", X_test.shape)
 
 
 if __name__ == "__main__":
--- src/train.py
+++ src/train.py
@@ -1,14 +1,37 @@
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 def split_data(X, y):
-
     X_train, X_test, y_train, y_test = train_test_split(
         X,
         y,
         test_size=0.20,
         random_state=42,
         stratify=y
-    )
+    ) #split in train and test data
 
     return X_train, X_test, y_train, y_test
+
+
+def scale_features(X_train, X_test):
+
+    numerical_columns = [
+        "Air temperature [K]",
+        "Process temperature [K]",
+        "Rotational speed [rpm]",
+        "Torque [Nm]",
+        "Tool wear [min]"
+    ] # scaling the continuous numerical features
+
+    scaler = StandardScaler()
+
+    X_train[numerical_columns] = scaler.fit_transform(
+        X_train[numerical_columns]
+    ) # learning from training data
+
+    X_test[numerical_columns] = scaler.transform(
+        X_test[numerical_columns]
+    ) # applying  scaling to test data
+
+    return X_train, X_test, scaler
